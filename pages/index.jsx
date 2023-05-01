1bfab06487084a2632341f3b4342bb01d0796905
Risbel Suarez <risbel961019@gmail.com>
2023-04-30 23:03:38 -0400

    feat: Add tailwind config

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>My exchange rate calculator</div>
+    <div className='text-red-500'>My exchange rate calculator</div>
   )
 }
 
